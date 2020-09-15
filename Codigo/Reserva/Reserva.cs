2b9fae87113e1b9e22d2a20e6d04f8d5a985afea
Torres911 <33793507+Torres911@users.noreply.github.com>
2020-09-14 20:04:57 -0500

    Update Reserva.cs

--- Codigo/Reserva/Reserva.cs
+++ Codigo/Reserva/Reserva.cs
@@ -1,40 +1,60 @@
-namespace Codigo.Salon{
+namespace Codigo.Reserva{
+    public class Reserva{
 
-    public class Salon{
-        
         #region Properties
 
         private int HoraInicio{ get; set; }
         private int HoraFinal{ get; set; }
-        private bool Luz{ get; set; }
-        private int Temperatura{ get; set; }
-        private List<Reserva> Reservas;
-        
+        private int Ocupante{ get; set; }
+        private int HoraPrenderLuz{ get; set; }
+        private int HoraApagarLuz{ get; set; }
+        private int HoraPrenderTem { get; set; }
+        private int HoraApagarTem { get; set; } 
+
         #endregion Properties
 
 
         #region Getters & Setters
-        
-        public string ID{
-            get { return ID; }
-            set { ID = value; }
+
+        public string HoraInicio{
+            get { return HoraInicio; }
+            set { HoraInicio = value; }
         }
-        
-        public string Estado{
-            get { return Estado; }
-            set { Estado = value; }
+
+        public string HoraFinal{
+            get { return HoraFinal; }
+            set { HoraFinal = value; }
         }
-        
-        public bool Luz{
-            get { return Luz; }
-            set { Luz = value; }
+
+        public bool Ocupante{
+            get { return Ocupante; }
+            set { Ocupante = value; }
         }
-        
+
         public int Temperatura{
             get { return Temperatura; }
             set { Temperatura = value; }
         }
-        
+
+        public int HoraPrenderLuz{
+            get { return HoraPrenderLuz; }
+            set { HoraPrenderLuz = value; }
+        }
+
+        public int HoraApagarLuz{
+            get { return HoraApagarLuz; }
+            set { HoraApagarLuz = value; }
+        }
+
+        public int HoraPrenderTem{
+            get { return HoraPrenderTem; }
+            set { HoraPrenderTem = value; }
+        }
+
+        public int HoraApagarTem{
+            get { return HoraApagarTem; }
+            set { HoraApagarTem = value; }
+        }
         #endregion Getters & Setters
     }
 }
